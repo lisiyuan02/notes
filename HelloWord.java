49aa4ca1b86da00accf91d2ee77767afe6aa8a5d
lisiyuan <lisiyuan02@126.com>
2019-11-18 20:26:55 +0800

    helloword2

---
+++ HelloWord.java
@@ -0,0 +1,9 @@
+public class HelloWord{
+	public static void main(String args[]){
+		System.out.println("Hello Word!");
+System.out.println("1");
+
+System.out.println("Hello Word!");
+System.out.println("2");
+
+}}
